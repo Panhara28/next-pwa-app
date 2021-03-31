9a49bbf718eb42b15d8423c81d3633d1824ff30a
rathpanha <rath.panha.rp@gmail.com>
2021-03-31 23:25:47 +0700

    improve seo

--- components/layout/SEO.tsx
+++ components/layout/SEO.tsx
@@ -13,7 +13,15 @@ const SEO: FunctionComponent<Props> = (props) => {
   return (
     <Head>
       <title>{(props.title ? props.title + " - " : "") + process.env.NEXT_PUBLIC_TITLE}</title>
+      {/* Metatag Data */}
+      <meta charSet='utf-8'/>
+      <meta name="Content-Type-Script" content="text/javascript" />
+      <meta name="Content-Type-Style" content="text/css" />
+      <meta name="description" content={props.description ? props.description : `Visit ${process.env.NEXT_PUBLIC_TITLE} for up-to-the-minute news, breaking news, and feature stories.`}/>	
+      <meta name="referrer" content="always" />
+      <link rel="canonical" href={process.env.NEXT_PUBLIC_URL + (props.pathname ? props.pathname : "")}/>
 
+      {/* Facebook Meta */}
       <meta property="og:url" content={process.env.NEXT_PUBLIC_URL + (props.pathname ? props.pathname : "")}/>
       <meta property="og:site_name" content={process.env.NEXT_PUBLIC_TITLE}/>
       <meta property="og:type" content={props.type ? props.type : "website"}/>
