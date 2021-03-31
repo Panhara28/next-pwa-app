8361c117a6f696aa611ae72c1c6b0a54f3775626
Rathpanha <rath.panha.rp@gmail.com>
2021-03-31 16:53:23 +0700

    testing theme render flip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -17,7 +17,7 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hanuman:wght@400;700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=block"/>
 
           {/* Theme Checking */}
-          <script src="/js/theme.js"/>
+          {/* <script src="/js/theme.js"/> */}
         </Head>
         
         <body>
