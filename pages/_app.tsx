b3251444a037a71be8aad647791780d91976c8cd
Rathpanha <rath.panha.rp@gmail.com>
2021-04-02 13:43:53 +0700

    google analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ApolloProvider } from "@apollo/client";
 import '../styles/globals.css';
 import '../styles/scss/main.scss';
 import useApollo from '../lib/apolloClient';
 import ThemeProvider from '../components/context/ThemeContext';
+import ReactGA from "react-ga";
+import { Router } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
+
+  useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    Router.events.on("routeChangeComplete", () => {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    });
+  }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
