ae48580005d7cba4e0751a6983d270644b8e63ee
rathpanha <rath.panha.rp@gmail.com>
2021-04-16 15:46:39 +0700

    improving meta tag

--- components/utilities/SEO.tsx
+++ components/utilities/SEO.tsx
@@ -11,31 +11,39 @@ type Props = {
 }
 
 const SEO: FunctionComponent<Props> = (props) => {
+  const url = process.env.NEXT_PUBLIC_URL + (props.pathname ? props.pathname : "");
+  const cononical = process.env.NEXT_PUBLIC_URL + (props.canonical ? props.canonical : "");
+  const siteName = process.env.NEXT_PUBLIC_TITLE;
+  const type = props.type ? props.type : "website";
+  const title = (props.title ? props.title + " - " : "") + process.env.NEXT_PUBLIC_TITLE;
+  const description = props.description ? props.description : `Visit ${process.env.NEXT_PUBLIC_TITLE} for up-to-the-minute news, breaking news, and feature stories.`;
+  const image = props.image ? props.image : "/assets/fallbackseo.jpg";
+
   return (
     <Head>
       <title>{(props.title ? props.title + " - " : "") + process.env.NEXT_PUBLIC_TITLE}</title>
       {/* Metatag Data */}
       <meta charSet='utf-8'/>
-      <meta name="Content-Type-Script" content="text/javascript" />
-      <meta name="Content-Type-Style" content="text/css" />
-      <meta 
-        name="description" 
-        content={props.description ? props.description : `Visit ${process.env.NEXT_PUBLIC_TITLE} for up-to-the-minute news, breaking news, and feature stories.`}
-      />	
+      <meta name="Content-Type-Script" content="text/javascript"/>
+      <meta name="Content-Type-Style" content="text/css"/>
+      <meta name="description" content={description}/>	
       <meta name="referrer" content="always" />
-      <link rel="canonical" href={process.env.NEXT_PUBLIC_URL + (props.canonical ? props.canonical : "")} key={"canonical"}/>
+      <link rel="canonical" href={cononical} key={"canonical"}/>
 
       {/* Facebook Meta */}
-      <meta property="og:url" content={process.env.NEXT_PUBLIC_URL + (props.pathname ? props.pathname : "")} key={"fb-url"}/>
-      <meta property="og:site_name" content={process.env.NEXT_PUBLIC_TITLE} key={"fb-site-name"}/>
-      <meta property="og:type" content={props.type ? props.type : "website"} key={"fb-type"}/>
-      <meta property="og:title" content={(props.title ? props.title + " - " : "") + process.env.NEXT_PUBLIC_TITLE} key={"fb-title"}/>
-      <meta 
-        property="og:description" 
-        content={props.description ? props.description : `Visit ${process.env.NEXT_PUBLIC_TITLE} for up-to-the-minute news, breaking news, and feature stories.`} 
-        key={"fb-description"}
-      />
-      <meta property="og:image" content={props.image ? props.image : "/assets/fallbackseo.jpg"} key={"fb-image"}/>
+      <meta property="og:url" content={url} key={"fb-url"}/>
+      <meta property="og:site_name" content={siteName} key={"fb-site-name"}/>
+      <meta property="og:type" content={type} key={"fb-type"}/>
+      <meta property="og:title" content={title} key={"fb-title"}/>
+      <meta property="og:description" content={description} key={"fb-description"}/>
+      <meta property="og:image" content={image} key={"fb-image"}/>
+
+      {/* Twitter Meta */}
+      <meta name="twitter:card" content="summary_large_image" key={"twitter-card"}/>
+      <meta name="twitter:site" content={siteName} key={"twitter-site"}/>
+      <meta name="twitter:title" content={title} key={"twitter-title"}/>
+      <meta name="twitter:description" content={description} key={"twitter-description"}/>
+      <meta name="twitter:image" content={image} key={"twitter-image"}/>
     </Head>
   );
 }
