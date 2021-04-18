6a824da1eb40b0e5758e23821bcae61a726e719e
rathpanha <rath.panha.rp@gmail.com>
2021-04-18 14:28:33 +0700

    fix now showing article sub category and loading icon

--- components/utilities/Loading.tsx
+++ components/utilities/Loading.tsx
@@ -3,9 +3,16 @@ import React from 'react';
 const Loading = ({ loading }: { loading: boolean }) => {
   return (
     <div className={`loadingio-wrapper ${(loading ? "show" : "")}`}> 
-      <div className="loadingio-spinner-ripple">
-        <div className="loadingio">
-          <div/><div/>
+      <div className="loadingio-spinner-double-ring">
+        <div className="loading-io">
+          <div/>
+          <div/>
+          <div>
+            <div/>
+          </div>
+          <div>
+            <div/>
+          </div>
         </div>
       </div>
     </div>
